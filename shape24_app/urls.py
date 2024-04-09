d3c3317691e23c287bc5ed271e654717e7e48051
Pranesh <me.praneshkumar@gmail.com>
2024-04-09 21:23:27 +0530

    updated urls

--- shape24_app/urls.py
+++ shape24_app/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path("", views.login, name="login"),
     path("login/", views.login, name="login"),
     path("dashboard/", views.dashboard, name="dashboard"),
     path(
