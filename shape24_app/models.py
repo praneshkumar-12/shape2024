2d6c3819fc1bc0f5edae2f14c771fbd50a794470
Pranesh <me.praneshkumar@gmail.com>
2024-04-09 16:57:44 +0530

    updated models

--- shape24_app/models.py
+++ shape24_app/models.py
@@ -4,36 +4,34 @@ from django.db import models
 class Users(models.Model):
     user_id = models.IntegerField(primary_key=True)
     email = models.CharField(max_length=255)
-    password = models.CharField(max_length=255, blank=True, null=True)
+    password = models.CharField(max_length=255)
 
     class Meta:
         managed = False
-        db_table = "users"
-        unique_together = (("user_id", "email"),)
+        db_table = 'users'
 
 
 class Projects(models.Model):
     project_id = models.IntegerField(primary_key=True)
     project_title = models.CharField(max_length=500)
-    availability = models.IntegerField(blank=True, null=True)
+    availability = models.IntegerField()
     description = models.TextField(blank=True, null=True)
     sdg = models.TextField(blank=True, null=True)
-    faculty_name = models.TextField(blank=True, null=True)
+    faculty_name = models.TextField()
     college_email = models.TextField(blank=True, null=True)
     mobile_number = models.TextField(blank=True, null=True)
-    department = models.TextField(blank=True, null=True)
+    department = models.TextField()
 
     class Meta:
         managed = False
-        db_table = "projects"
-        unique_together = (("project_id", "project_title"),)
+        db_table = 'projects'
 
 
 class AssignedProjects(models.Model):
-    user = models.OneToOneField("Users", models.DO_NOTHING, primary_key=True)
-    project = models.ForeignKey("Projects", models.DO_NOTHING)
+    user = models.ForeignKey('Users', models.DO_NOTHING, primary_key=True)
+    project = models.ForeignKey('Projects', models.DO_NOTHING)
 
     class Meta:
         managed = False
-        db_table = "assigned_projects"
-        unique_together = (("user", "project"),)
+        db_table = 'assigned_projects'
+        unique_together = (('user', 'project'),)
