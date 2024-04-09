db14fea5af73848f5b6fe40f93f700678ae703ca
Pranesh <me.praneshkumar@gmail.com>
2024-04-09 16:35:47 +0530

    debug

--- shape2024/settings.py
+++ shape2024/settings.py
@@ -24,9 +24,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [os.environ["WEBSITE_HOSTNAME"]]
+
+ADMINS = [("Debug", os.environ["FROM_EMAIL"])]
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = os.environ["FROM_EMAIL"]
+EMAIL_HOST_PASSWORD = os.environ["EMAIL_KEY"]
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 
 # Application definition
